7cb8f62c73d56316fd3eac822f082e62378ffe6b
Konvx <74506382+Konvx@users.noreply.github.com>
2021-05-13 20:16:11 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.get('/:file', (req, res) => {
+    res.sendFile(__dirname + '/' + req.params.file)
+})
+
+app.listen(port, () => console.log(`App is listening at port ${port}`))
