276bec0bd5999517929183d27bb366c3fbb29657
Konvx <74506382+Konvx@users.noreply.github.com>
2021-05-13 20:20:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
 app.get('/:file', (req, res) => {
-    res.sendFile(__dirname + '/' + req.params.file)
+    res.sendFile(dirname + '/' + req.params.file)
 })
 
 app.listen(port, () => console.log(`App is listening at port ${port}`))
